c07d2fd991f74526a45eb2ab87c4520ad790c3ac
r.totoki <r.totoki@NP-21-095-O.shantery.local>
2022-07-26 15:37:57 +0900

    コミット十時1回目

---
+++ src/main/java/com/shantery/result2/Result2Application.java
@@ -0,0 +1,13 @@
+package com.shantery.result2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Result2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Result2Application.class, args);
+	}
+
+}
