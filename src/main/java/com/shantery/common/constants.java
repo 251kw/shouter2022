2b64cfcfc5d749dd2373f66e258a3fb454d847c1
h.kasai <h.kasai@NP-21-096-O.shantery.local>
2022-07-29 15:49:43 +0900

    第1回コミット追加（葛西）

--- src/main/java/com/shantery/common/constants.java
+++ src/main/java/com/shantery/common/constants.java
@@ -1,6 +1,13 @@
 package com.shantery.common;
 
 public class constants {
+	//name属性名
+	public static final String loginId = "loginId";
+	public static final String userName = "userName";
+	public static final String password = "password";
+	public static final String icon = "icon";
 	public static final String iconMale = "icon-user";
 	public static final String iconFemale = "icon-user-female";
+	public static final String profile = "profile";
+	public static final String writing = "writing";
 }
