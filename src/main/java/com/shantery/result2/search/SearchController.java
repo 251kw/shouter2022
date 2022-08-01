880c1bafe1d965bd0afc7581b8cb026d9c28013f
s.ogata <s.ogata@NP-22-110-O.shantery.local>
2022-08-01 15:03:35 +0900

    検索機能コントローラー(小方)

--- src/main/java/com/shantery/result2/search/SearchController.java
+++ src/main/java/com/shantery/result2/search/SearchController.java
@@ -1,5 +1,46 @@
 package com.shantery.result2.search;
 
-public class SearchController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+@Controller
+public class SearchController {
+	
+	//index.htmlから送信されてくるのでpost
+	@RequestMapping(value="/",method=RequestMethod.GET)
+	public ModelAndView serchInput(ModelAndView mav) {
+		mav.setViewName("UserSearchInput");
+		mav.addObject("check", "noCheck");
+		return mav;
+	}
+	
+	//検索条件が入力されたらpostで受け取る
+	//追加すること(仮)　・バリデーションチェック、検索メソッド
+	@RequestMapping(value="/",method=RequestMethod.POST)
+	public ModelAndView send(@RequestParam("loginId")String loginId,
+			@RequestParam("userName")String userName,@RequestParam("icon1")String icon1,
+			@RequestParam("icon2")String icon2,@RequestParam("profile")String profile, ModelAndView mav) {
+		//検索条件の保持
+		mav.setViewName("UserSearchInput");
+		mav.addObject("loginId",loginId);
+		mav.addObject("userName", userName);
+		String iconCheck = null;
+		if(icon1=="icon-male" && icon2=="icon-female") {
+			iconCheck = "checks";
+		}else {
+			if(icon1=="icon-male") {
+				iconCheck = icon1;
+			}else if(icon2=="icon-female") {
+				iconCheck = icon2;
+			}else {
+				iconCheck = "noCheck";
+			}
+		}
+		mav.addObject("check", iconCheck);
+		mav.addObject("profile", profile);
+		return mav;
+	}
 }
