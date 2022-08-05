d74ec9d2e86d5c115c2958298c90c5cf596e8d21
s.ogata <s.ogata@NP-22-110-O.shantery.local>
2022-08-05 11:27:52 +0900

    ログインコントローラー(小方)

--- src/main/java/com/shantery/result2/login/LoginController.java
+++ src/main/java/com/shantery/result2/login/LoginController.java
@@ -6,20 +6,35 @@ import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 
-
-/**
- * @author y.nakaya
- *
- */
 @Controller
 public class LoginController {
+	
+	//index.htmlへ接続
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public ModelAndView index(ModelAndView mav) {
 		mav.setViewName("index");
 		return mav;
 	}
+	
+	@RequestMapping(value = "/Info", method = RequestMethod.POST)
+	public ModelAndView UserInfoInput(ModelAndView mav) {
+		mav.setViewName("UserInfoInput");
+		return mav;
+	}
+	
+	@RequestMapping(value="", method=RequestMethod.POST)
+	public ModelAndView shouter(@RequestParam(value="lodinId",required=true)String loginId,
+								@RequestParam(value="password",required=true)String password,
+								ModelAndView mav) {
+		
+		
+		return mav;
+	}
+	
+	
+	
 }
